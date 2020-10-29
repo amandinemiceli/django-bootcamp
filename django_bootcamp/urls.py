e99d46ccc403a3e3d4a922ac02845922b6c74c9c
Amandine Miceli <amandine.miceli@job2day.fr>
2020-10-29 09:52:28 +0100

    Started Products view creation with data from DB

--- django_bootcamp/urls.py
+++ django_bootcamp/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from products import views
+
 urlpatterns = [
+    path('search/', views.home_view),
+    path('products/1/', views.product_detail_view),
+    path('api/products/1/', views.product_api_detail_view),
     path('admin/', admin.site.urls),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+from products.models import Product
+
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World!</h1>")
+
+
+def product_detail_view(request, *args, **kwargs):
+    obj = Product.objects.get(id=1)
+    return HttpResponse(f"Product id {obj.id}")
+
+
+def product_api_detail_view(request, *args, **kwargs):
+    obj = Product.objects.get(id=1)
+    return JsonResponse({"id": obj.id})
+
