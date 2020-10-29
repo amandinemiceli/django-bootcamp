4510b2d486466b71b0d5af2b9e085d01b5291ff5
Amandine Miceli <amandine.miceli@job2day.fr>
2020-10-29 10:32:35 +0100

    Added handling Exceptions in Products views

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.shortcuts import render
 
 
@@ -10,12 +10,18 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World!</h1>")
 
 
-def product_detail_view(request, *args, **kwargs):
-    obj = Product.objects.get(id=1)
+def product_detail_view(request, pk):
+    try:
+        obj = Product.objects.get(id=pk)
+    except Product.DoesNotExist:
+        raise Http404 # render html page, with HTTP status code of 404
     return HttpResponse(f"Product id {obj.id}")
 
 
-def product_api_detail_view(request, *args, **kwargs):
-    obj = Product.objects.get(id=1)
+def product_api_detail_view(request, pk, *args, **kwargs):
+    try:
+        obj = Product.objects.get(id=pk)
+    except Product.DoesNotExist:
+        return JsonResponse({"message": "Not found"}) # return JSON with HTTP status code of 404
     return JsonResponse({"id": obj.id})
 
