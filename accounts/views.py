41aa94cfe96f3d4163d860b28b005adc92dec1c1
Amandine Miceli <amandine.miceli@job2day.fr>
2020-11-01 14:59:45 +0100

    Reformatted login view a little

--- accounts/views.py
+++ accounts/views.py
@@ -11,12 +11,12 @@ def login_view(request, *args, **kwargs):
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
         user = authenticate(request, username=username, password=password)
-        if user is None:
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
             # attempt = request.session.get('attempt') or 0
             # request.session['attempt'] = attempt + 1
             # return redirect("/invalid-password")
             request.session['invalid_user'] = 1
-            return render(request, "forms.html", {"form": form})
-        login(request, user)
-        return redirect("/")
     return render(request, "forms.html", {"form": form})
