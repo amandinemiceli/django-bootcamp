c40ef7e4d56dbc0c2277fd0be4eafffa6fed06d1
Amandine Miceli <amandine.miceli@job2day.fr>
2020-11-02 22:44:32 +0100

    Started to write TDD to verify that user exists or password is correct

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+User = get_user_model()
+
+
 # Create your tests here.
+class UserTestCase(TestCase):
+
+    def setUp(self): # Python's built-in unit test
+        user_a = User(username='Emma', email='emma@example.com')
+        # User.objects.create()
+        # User.objects.create_user
+        user_a_pw = 'some_123_password'
+        self.user_a_pw = user_a_pw
+        user_a.is_staff = True
+        user_a.is_superuser = True
+        user_a.save()
+        user_a.set_password(user_a_pw)
+        self.user_a = user_a
+
+    def test_user_exists(self):
+        user_count = User.objects.all().count()
+        self.assertEqual(user_count, 1)
+        self.assertNotEqual(user_count, 0)
+
+    def test_user_password(self):
+        self.assertTrue(self.user_a.check_password(self.user_a_pw))
